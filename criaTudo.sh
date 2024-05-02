656c501ef67334a73dcc3ac5ecbbc65c02f3d2ee
glaide <glaide@assis.co>
2024-05-01 23:39:51 -0300

    update criatudo

--- criaTudo.sh
+++ criaTudo.sh
@@ -1,20 +1,23 @@
 rm -f Tabelas.sqlite3
-echo -n "Criando novos amigos"
+echo -n "Criando novos amigos. "
 ruby amigo/criaAmigos.rb
 echo "Novos amigos criados com sucesso."
 
-echo -n "Criando novas casas."
+echo -n "Criando novas casas. "
 ruby casa/criaCasas.rb
 echo "Novas casas criadas com sucesso."
 
-echo -n "Criando novas pessoas."
+echo -n "Criando novas pessoas. "
 ruby pessoa/criaPessoas.rb
 echo "Novas pessoas criadas com sucesso."
 
-echo -n "Criando novos resumos."
+echo -n "Criando novos resumos. "
 ruby resumo/criaResumos.rb
 echo "Novos resumos criados com sucesso."
 
+echo -n "Criando amigos e pessoas. "
+ruby amigo/criaAmigosPessoas.rb
+echo "Amigos e pessoas criados com sucesso."
 
 
 
